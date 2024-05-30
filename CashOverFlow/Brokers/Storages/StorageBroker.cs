c0577c916ba8f6ebec7ad2ec06c323e941f3cb15
Azizbek Askarov <azizasqarov773@gmail.com>
2024-05-30 15:10:18 +0500

    CONFIGS: Register Db Context

---
+++ CashOverFlow/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,23 @@
+﻿using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace CashOverFlow.Brokers.Storages;
+
+public partial class StorageBroker : EFxceptionsContext
+{
+    private readonly IConfiguration configuration;
+
+    public StorageBroker(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+        this.Database.Migrate();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string connectionString = this.configuration.GetConnectionString(name: "DefaultConnection");
+
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+}
