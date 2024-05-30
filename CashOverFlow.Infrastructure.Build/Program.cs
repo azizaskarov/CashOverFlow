8788bae35dd5633c6adc037f3493250a4084964a
Azizbek Askarov <azizasqarov773@gmail.com>
2024-05-30 11:59:16 +0500

    Add project files.

---
+++ CashOverFlow.Infrastructure.Build/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+var githubPipeline = new GithubPipeline
+{
+    Name = "Build & Test CashOverflow",
+
+    OnEvents = new Events
+    {
+        Push = new PushEvent
+        {
+            Branches = new string[] { "master" }
+        },
+
+        PullRequest = new PullRequestEvent
+        {
+            Branches = new string[] { "master" }
+        }
+    },
+
+    Jobs = new Jobs
+    {
+        Build = new BuildJob
+        {
+            RunsOn = BuildMachines.WindowsLatest,
+
+            Steps = new List<GithubTask>()
+            {
+                new CheckoutTaskV2()
+                {
+                    Name = "Checking out"
+                },
+
+                new SetupDotNetTaskV1()
+                {
+                    Name = "Installing .NET",
+
+                    TargetDotNetVersion = new TargetDotNetVersion()
+                    {
+                        DotNetVersion = "7.0.200"
+                    }
+                },
+
+                new RestoreTask()
+                {
+                    Name= "Restoring packages"
+                },
+
+                new DotNetBuildTask()
+                {
+                    Name ="Building project"
+                },
+
+                new TestTask()
+                {
+                    Name = "Running tests"
+                }
+            }
+        }
+    }
+};
+
+var aDotnetClient = new ADotNetClient();
+aDotnetClient.SerializeAndWriteToFile(
+    githubPipeline,
+    path: "fwrg");
