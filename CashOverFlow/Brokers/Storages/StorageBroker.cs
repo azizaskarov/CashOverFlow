2999106df3877e1aee2707848ef53816bc004fc7
Azizbek Askarov <azizasqarov773@gmail.com>
2024-05-31 11:36:29 +0500

    BROKERS: Created Loacations Broker

--- CashOverFlow/Brokers/Storages/StorageBroker.cs
+++ CashOverFlow/Brokers/Storages/StorageBroker.cs
@@ -1,10 +1,12 @@
-﻿using EFxceptions;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using EFxceptions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 namespace CashOverFlow.Brokers.Storages;
 
-public partial class StorageBroker : EFxceptionsContext
+public partial class StorageBroker : EFxceptionsContext, IStorageBroker
 {
     private readonly IConfiguration configuration;
 
@@ -14,10 +16,52 @@ public partial class StorageBroker : EFxceptionsContext
         this.Database.Migrate();
     }
 
+    public async ValueTask<T> InsertAsync<T>(T @object)
+    {
+        var broker = new StorageBroker(this.configuration);
+        broker.Entry(@object).State = EntityState.Added;
+        await broker.SaveChangesAsync();
+
+        return @object;
+    }
+
+    public async ValueTask<T> SelectAsync<T>(params object[] objectIds) where T : class
+    {
+        var broker = new StorageBroker(this.configuration);
+
+        return await broker.FindAsync<T>(objectIds);
+    }
+
+    public IQueryable<T> SelectAll<T>() where T : class
+    {
+        var broker = new StorageBroker(configuration);
+
+        return broker.Set<T>();
+    }
+
+    public async ValueTask<T> UpdateAsync<T>(T @object)
+    {
+        var broker = new StorageBroker(this.configuration);
+        broker.Entry(@object).State = EntityState.Modified;
+        await broker.SaveChangesAsync();
+
+        return @object;
+    }
+
+    public async ValueTask<T> DeleteAsync<T>(T @object)
+    {
+        var broker = new StorageBroker(this.configuration);
+        broker.Entry(@object).State = EntityState.Deleted;
+        await broker.SaveChangesAsync();
+
+        return @object;
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         string connectionString = this.configuration.GetConnectionString(name: "DefaultConnection");
 
         optionsBuilder.UseSqlServer(connectionString);
     }
+
 }
