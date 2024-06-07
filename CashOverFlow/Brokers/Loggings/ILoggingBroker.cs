c1cd970d101891d8a820535ce389690628f84e5d
Azizbek Askarov <azizasqarov773@gmail.com>
2024-06-07 22:15:29 +0500

    CONFIGS: Register Logging

---
+++ CashOverFlow/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CashOverFlow.Brokers.Loggings;
+
+public interface ILoggingBroker
+{
+    void LogError(Exception  exception);
+    void LogCritical(Exception  exception);
+}
